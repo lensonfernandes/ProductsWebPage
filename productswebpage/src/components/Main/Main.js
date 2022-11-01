c70cefbf6dd30691009f2dd56b772ac4f32fc1f0
lensonfernandes <lenson.fernand@gmail.com>
2022-11-01 23:10:35 +0530

    Main heading left alignment fixed

--- productswebpage/src/components/Main/Main.js
+++ productswebpage/src/components/Main/Main.js
@@ -7,7 +7,7 @@ function Main() {
     <div className='main-container'>
       <div className='m-c-left-container'>
         <h5>SUMMER 2020</h5>
-        <h1>NEW COLLECTION</h1>
+        <h1 className='main-heading-h1'>NEW COLLECTION</h1>
         <h4>We know how large objects will act, 
 but things on a small scale.</h4>
         <button>SHOP NOW</button>
